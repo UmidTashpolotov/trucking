8b8c23c92476a5a5e44608d61f23f1dbeb91fa72
Umid <umid.tashpolotov@gmail.com>
2017-04-10 13:29:12 +0600

    #3 Добавил в сиды 2-х пользователей

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'John', surname: 'Doe', phone: '0555 555 555')
+User.create(name: 'James', surname: 'Bond', phone: '0707 007 007')
