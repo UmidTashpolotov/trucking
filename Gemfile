7e32f1bf43bb7bc1271e5644509a146a2dfc9fc2
ElmiraMatyeva <elmira.matyeva@gmail.com>
2017-04-11 15:16:49 +0600

    #5. Добавила gem Devise и gem Devise Bootstrapped. Добавила в Navbar ссылки на вход/выход пользователя, а также надпись hello, current_user.email

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'devise'
+gem 'devise-bootstrapped'
 gem 'bootstrap-sass'
 gem 'simple_form'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	validates :role, :inclusion=> { :in => ['customer', 'admin', 'worker'] }
   has_many :cars
   has_many :documents
