bb3fcd679e502029bc9189ef53f6d16e722a1c99
Sergei Balabanov <sergeibalabanov@gmail.com>
2017-05-16 14:48:15 +0600

    #80 Подправил скрипт создания билда.

--- scripts/create_deployment_package.sh
+++ scripts/create_deployment_package.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+directory=pwd
+
+mkdir -p ${directory}/artifacts
+
 build_number=${1}
 
 RAILS_ENV=production rake assets:precompile
 
 echo ${build_number} > VERSION
-
-mkdir artifacts
 
 tar -czf ${HOME}/artifacts/trucking_${build_number}.tar.gz \
 --exclude=README.md \
