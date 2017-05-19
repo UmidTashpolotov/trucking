be7d66d9a993e89441f882ff7bfe86b157feaf77
Sergei Balabanov <sergeibalabanov@gmail.com>
2017-05-19 18:38:10 +0600

    #80 Переписал скрипт поставки авхива с обновлением.

--- scripts/deploy_update_package.sh
+++ scripts/deploy_update_package.sh
@@ -1,16 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
-host={1}
-id_rsa_key_path=${2}
-instance_name=${3}
-build_number=${4}
+# sh 0 $script 1 $ssh_remote_host 2 $keys_folder 3 $ssh_key 4 demo 5 $PACKAGE_BUILD_NUMBER
 
+host=${1}
+keys_folder=${2}
+ssh_key=${3}
+instance_name=${4}
+build_number=${5}
 package_name=trucking_${build_number}.tar.gz
 package_name_path=${HOME}/artifacts/${package_name}
 
-chmod 700 keys
-chmod 600 ${id_rsa_key_path}
+chmod 700 ${keys_folder}
+chmod 600 ${ssh_key}
 
-scp -i ${id_rsa_key_path} ${package_name_path} ${host}:~
+scp -i ${ssh_key} -p scripts/apply_update.sh ${host}:~
+scp -i ${ssh_key} ${package_name_path} ${host}:~
+
+ssh -i ${ssh_key} ${host} << EOL
+chmod +x apply_update.sh
+sudo ./apply_update.sh ${instance_name} ${package_name}
+EOL
