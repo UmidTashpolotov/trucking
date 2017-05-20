67b4970a7aad08d137b1eddea4f2749bb784dcb2
Sergei Balabanov <sergeibalabanov@gmail.com>
2017-05-20 20:15:23 +0600

    #80 Работаю над скриптом развёртывания.

--- scripts/apply_update.sh
+++ scripts/apply_update.sh
@@ -9,29 +9,28 @@ fi
 
 project_root=${HOME}/web_app
 instance_name=${1}
-package_name=${2}
+package_name=${HOME}/${2}
 
-service nginx stop             # Остановка сервисов
+# Остановка сервисов
 service unicorn_trucking stop
 
 cd ~
 mkdir -p old_builds
 
 cd web_app
-tar -czf ${HOME}/old_builds/trucking_previous.tar.gz \ # Создание бекапа
---exclude=README.md \
-.
+tar -czf ${HOME}/old_builds/trucking_previous.tar.gz . # Создание бекапа
 
 cd ~
-tar -xvzf package_name -C web_app # Распаковка приложения
-RAILS_ENV=production
-rake db:migrate
+tar -xvzf $package_name -C web_app # Распаковка приложения
+cd web_app
+mkdir -p shared/sockets
+RAILS_ENV=production bundle install
+RAILS_ENV=production rake db:setup
 
 # Установка зависимостей
 # Запуск миграций и свойственных для приложения конфигураций
-service unicorn_trucking start  # Запуск сервисов
-service nginx start
+
+# Запуск сервисов
+service unicorn_trucking start
+
 # Зачистка, удаление этого скрипта и архива
-
-
-
