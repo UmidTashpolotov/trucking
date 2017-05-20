fa5ced5b5e5a17cbb886452b42dd33bd8f27ae5e
Sergei Balabanov <sergeibalabanov@gmail.com>
2017-05-20 16:47:36 +0600

    #80 Дописал скрипт поставки обновления.

--- scripts/apply_update.sh
+++ scripts/apply_update.sh
@@ -11,14 +11,22 @@ project_root=${HOME}/web_app
 instance_name=${1}
 package_name=${2}
 
-cd ~
-mkdir lolkek
-
 service nginx stop             # Остановка сервисов
 service unicorn_trucking stop
 
-# Создание бекапа
-# Распаковка приложения
+cd ~
+mkdir -p old_builds
+
+cd web_app
+tar -czf ${HOME}/old_builds/trucking_previous.tar.gz \ # Создание бекапа
+--exclude=README.md \
+.
+
+cd ~
+tar -xvzf package_name -C web_app # Распаковка приложения
+RAILS_ENV=production
+rake db:migrate
+
 # Установка зависимостей
 # Запуск миграций и свойственных для приложения конфигураций
 service unicorn_trucking start  # Запуск сервисов
