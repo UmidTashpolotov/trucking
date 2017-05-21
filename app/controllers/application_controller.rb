416977854fd707ac04e8f51ac03b3e428ed86191
Sergei Balabanov <sergeibalabanov@gmail.com>
2017-05-22 02:55:54 +0600

    #80 Сделал вывод версии проекта в навбаре.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_project_version
 
   def after_sign_in_path_for(resource)
     if current_user.role == "admin"
@@ -9,4 +10,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def set_project_version
+    @project_version = File.read("VERSION") if File.exist?("VERSION")
+  end
 end
