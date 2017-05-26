4df63a59ca7b3acdb652fd3489317c819fd4c9c7
Sergei Balabanov <sergeibalabanov@gmail.com>
2017-05-26 18:19:01 +0600

    #59 Написал тестирование оставления отзыва водителем.

--- features/step_definitions/users_rating.rb
+++ features/step_definitions/users_rating.rb
@@ -7,3 +7,26 @@ end
 When(/^перевозчик выполнил работу$/) do
   @order.complete
 end
+
+When(/^увидит форму оставления отзыва$/) do
+  assert page.has_content?('Оставьте комментарий')
+end
+
+When(/^он оставит отзыв "([^"]*)" оценка "([^"]*)"$/) do |message, rating|
+  fill_in('comment_body', with: message)
+  select(rating)
+  click_on('create_comment')
+end
+
+When(/^отзыв "([^"]*)" оценка "([^"]*)" будет виден на странице заказа$/) do |message, rating|
+  assert page.has_content?(message)
+  assert page.has_content?(rating)
+end
+
+When(/^водитель заходит в систему$/) do
+  visit('/')
+  click_on('log_in')
+  fill_in('Телефон', with: @worker.phone)
+  fill_in('Пароль', with: @worker.password)
+  click_on('log_in_button')
+end
