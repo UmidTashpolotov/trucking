7daf8baef676bcffc3d189c660fbfec990a9293e
Alexander Medvedev <violent.wetra@gmail.com>
2017-06-12 16:00:32 +0600

    #111 Вынес форму подтверждения телефона в application layout, в зависимости от роли пользователя выводится сообщение о невозможности создания Order/Offer.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,11 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update(user_params)
-        format.html {redirect_to my_profile_path, notice: 'Ваш профиль успешно обновлен'}
-        format.json {render :show, status: :ok, location: @user}
+        format.html { redirect_to my_profile_path, notice: 'Ваш профиль успешно обновлен' }
+        format.json { render :show, status: :ok, location: @user }
       else
-        format.html {render :edit}
-        format.json {render json: @user.errors, status: :unprocessable_entity}
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
