f7eec11ab2b433a22be903b77a1205146e1980ba
Sergei Balabanov <sergeibalabanov@gmail.com>
2017-06-14 20:44:53 +0600

    #113 Сделал алерт при создании bid уведомляющий о том что не все поля заполнены..

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,10 @@
 class Bid < ApplicationRecord
+  validates :from_city, presence: true
+  validates :to_city, presence: true
+  validates :weight, presence: true
+  validates :volume, presence: true
+  validates :phone, presence: true
+
   def close
     self.status = 'old'
     save
