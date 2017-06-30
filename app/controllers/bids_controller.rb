fe692eeafb5113acc7ad57fc2d1f76fc3ba486cd
Sergey Balabanov <sergeibalabanov@gmail.com>
2017-07-01 02:35:01 +0600

    #121 Добавил отображение ошибок при создании Bid. Добавил окно уведомляющее об успешном создании bid.

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,14 +1,23 @@
 class BidsController < ApplicationController
 
   def new
+    @bid = Bid.new
   end
 
   def create
     @bid = Bid.new(bid_params)
     if @bid.save
-      redirect_back(fallback_location: root_path, notice: t('bid_created'))
+      if user_signed_in?
+        render 'success'
+      else
+        redirect_back(fallback_location: root_path, notice: t('bid_created'))
+      end
     else
-      redirect_back(fallback_location: root_path, notice: 'Заполните все поля.')
+      if user_signed_in?
+        render 'new'
+      else
+        redirect_back(fallback_location: root_path, notice: 'Заполните все поля.')
+      end
     end
   end
 
